b5c3a0f8b05d8a0da60530324ce516fb1c64d5d3
Michael R. Bechtel <mikebec20@gmail.com>
2017-08-24 23:44:28 -0400

    route updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -21,9 +21,52 @@ module.exports = function(app){
   // object in the friends.js file
 
   app.post("/api/friends", function(req, res){
-    friends.push(req.body);
 
-    
+    var newUserName = req.body.name;
+    var newUserPic = req.body.photo;
+
+    var newUserScores = req.body.scores.map(function(item){
+
+      return parseInt(item, 10)
+    });
+
+    console.log(newUserScores);
+
+    friends.push(
+      {
+        name: newUserName,
+        photo: newUserPic,
+        scores: newUserScores
+      }
+    );
+
+    console.log(friends);
+
+    var difference;
+    var sum = 0;
+    var differences = [];
+    var score1 = [];
+    var score2 = [];
+
+
+    //No clue how to get this logice to work...
+
+    for (var i = 0; i < friends.length; i++) {
+      for (var j = 0; j < friends[i].scores.length; j++) {
+
+        score1 = newUserScores[j];
+        console.log("\nNew User Score for Question " + [j+1] + ": " + score1);
+        score2 = friends[i].scores[j];
+        console.log("Friends Array Score for Question " + [j+1] + ": " + score2);
+
+        difference = Math.abs(score1 - score2);
+        sum += difference;
+        console.log(difference);
+      }//end inner loop
+
+      console.log(sum);
+
+    }//end outer loop
 
     res.json(friends);
   });
